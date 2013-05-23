1e0bdacacb1ccdfa433b7c64880b97d379d711b6
Markus Fasselt <markus.fasselt@gmail.com>
2013-05-23 18:02:14 +0200

    FieldContent

--- field.c
+++ field.c
@@ -3,50 +3,6 @@
 #include "field.h"
 
 /**
- * Repräsentiert das Spielfeld
- *
- */
-struct PlayingField {
-	int width;
-	int height;
-
-	struct FieldContent *population; // Belegung der einzelnen Felder des Spielfelds
-	int numberOfPredators; // Anzahl Räuber aktuell
-	int numberOfPreys; // Anzahl Beute aktuell
-};
-
-/**
- * die Belegung eines Feldes
- *
- */
-struct FieldContent {
-	enum FieldPopulationType *populationType;
-
-	// wenn populationType == EMPTY muss keines dieser Werte gesetzt werden
-	int age; // erreicht das Lebewesen ein bestimmtes Alter, bringt es ein neues Lebewesen auf die Welt
-
-	int starveTime; // Anzahl der Zeiteinheiten, die der Räuber hungern muss (nur für Räuber)
-					// nach x ZE stirbt der Räuber
-
-};
-
-/**
- * die möglichen Feldbelegungen: leeres Feld, Räuber & Beute
- *
- */
-enum FieldPopulationType {
-	EMPTY, PREDATOR, PREY
-};
-
-/**
- * Richtungen in die sich ein Tier bewegen kann
- *
- */
-enum Direction {
-	TOP, BOTTOM, LEFT, RIGHT
-};
-
-/**
  * 	Initialisiert das Spielfeld mit der angebenen Breite und Höhe
  *
  */
@@ -56,7 +12,15 @@ void initField(int width, int height) {
 	field->height = height;
 	field->numberOfPredators = 0;
 	field->numberOfPreys = 0;
-	field->population = malloc(sizeof(struct FieldContent) * width * height);
+	field->population = malloc(sizeof(struct FieldContent*) * width * height);
+
+	for (int i = 0; i < field->width; i++) {
+		for (int j = 0; j < field->height; j++) {
+
+			struct FieldContent *cont = getFieldContent(field, i, j);
+			cont->age = 0;
+		}
+	}
 
 	printf("Initializing a %dx%d field\n", field->width, field->height);
 
@@ -64,6 +28,7 @@ void initField(int width, int height) {
 }
 
 void dumpPopulation(struct PlayingField *field) {
+//	printf("%d", field->population[0][0]->age);
 //	for (int i = 0; i < field->width; i++) {
 //		for (int j = 0; j < field->height; j++) {
 //			printf("%d", field->population[i][j]->age);
@@ -77,4 +42,8 @@ void dumpPopulation(struct PlayingField *field) {
  */
 void simulationStep(struct PlayingField *field) {
 
+}
+
+void* getFieldContent(struct PlayingField *field, int x, int y) {
+	return &(*((*(field->population + x)) + y));
 }
--- field.h
+++ field.h
@@ -1,6 +1,55 @@
 #ifndef FIELD_H_INCLUDED
 #define FIELD_H_INCLUDED
 
+
+/**
+ * Repräsentiert das Spielfeld
+ *
+ */
+struct PlayingField {
+	int width;
+	int height;
+
+	struct FieldContent **population; // Belegung der einzelnen Felder des Spielfelds
+	int numberOfPredators; // Anzahl Räuber aktuell
+	int numberOfPreys; // Anzahl Beute aktuell
+};
+
+/**
+ * die Belegung eines Feldes
+ *
+ */
+struct FieldContent {
+	enum FieldPopulationType *populationType;
+
+	// wenn populationType == EMPTY muss keines dieser Werte gesetzt werden
+	int age; // erreicht das Lebewesen ein bestimmtes Alter, bringt es ein neues Lebewesen auf die Welt
+
+	int starveTime; // Anzahl der Zeiteinheiten, die der Räuber hungern muss (nur für Räuber)
+					// nach x ZE stirbt der Räuber
+
+};
+
+/**
+ * die möglichen Feldbelegungen: leeres Feld, Räuber & Beute
+ *
+ */
+enum FieldPopulationType {
+	EMPTY, PREDATOR, PREY
+};
+
+/**
+ * Richtungen in die sich ein Tier bewegen kann
+ *
+ */
+enum Direction {
+	TOP, BOTTOM, LEFT, RIGHT
+};
+
 void initField(int width, int height);
+
+void* getFieldContent(struct PlayingField *field, int x, int y);
+
+void dumpPopulation(struct PlayingField *field);
 
 #endif
