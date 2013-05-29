ad93e73f128a9a3d423a59270048ae717c25072d
Markus Fasselt <markus.fasselt@gmail.com>
2013-05-29 14:03:21 +0200

    bitmap

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "map.h"
 #include "simulation.h"
-
 
 /*
  * Applikation zur Simmulation einer Räuber-Beute-Beziehung
@@ -15,5 +15,22 @@ int main() {
 
 	initField(20, 20);
 
+	createBitmap();
+
 	return 0;
+}
+
+void createBitmap() {
+	int width = 1000;
+	int height = 1000;
+
+	char *bmp;
+	bmp = malloc(sizeof(char) * width * height * 3);
+
+	for(int i = 0; i < width * height * 3; i++)
+	{
+		bmp[i] = rand() % 256;
+	}
+
+	write_bmp("/tmp/test.bmp", width, height, bmp);
 }
